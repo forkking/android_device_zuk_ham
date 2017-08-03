6360c8f5149f81163560b1ee38592bac7a730fcc
sb6596 <shubhamprince111@gmail.com>
2017-08-03 15:21:11 +0000

    WIP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,7 +188,7 @@ BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 TARGET_USES_WCNSS_CTRL           := true
-TARGET_USES_QCOM_WCNSS_QMI       := false
+TARGET_USES_QCOM_WCNSS_QMI       := true
 TARGET_USES_WCNSS_MAC_ADDR_REV   := true
 
 # Inherit from QC proprietary
--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -31,8 +31,8 @@ endif
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include/media/openmax \
-        hardware/qcom/display/msm8974/libgralloc \
-        hardware/qcom/media/msm8974/libstagefrighthw \
+        hardware/qcom/display-caf-msm8974/libgralloc \
+        hardware/qcom/media-caf-msm8974/libstagefrighthw \
         $(LOCAL_PATH)/../../mm-image-codec/qexif \
         $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
         $(LOCAL_PATH)/../util \
